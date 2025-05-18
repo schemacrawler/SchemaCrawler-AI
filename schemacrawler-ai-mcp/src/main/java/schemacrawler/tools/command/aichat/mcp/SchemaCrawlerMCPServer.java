d36bc8fc20a8625f13a3af67afc20bcc91c2f120
Sualeh Fatehi <sualeh@hotmail.com>
2025-05-18 10:10:54 -0400

    Add tool

--- schemacrawler-ai-mcp/src/main/java/schemacrawler/tools/command/aichat/mcp/SchemaCrawlerMCPServer.java
+++ schemacrawler-ai-mcp/src/main/java/schemacrawler/tools/command/aichat/mcp/SchemaCrawlerMCPServer.java
@@ -1,5 +1,7 @@
 package schemacrawler.tools.command.aichat.mcp;
 
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,4 +15,15 @@ public class SchemaCrawlerMCPServer {
   public static void main(String[] args) {
     SpringApplication.run(SchemaCrawlerMCPServer.class, args);
   }
+
+  @Tool(name = "get-weather", description = "Get the weather in a city", returnDirect = true)
+  public String weather(@ToolParam(description = """
+    Name of the city, including state and country.
+    For example, Boston, MA, USA
+    """, required = true) final String city) {
+    String message = String.format("The weather in %s is sunny", city);
+    System.out.printf(">>> %s", message);
+    return message;
+  }
+
 }
