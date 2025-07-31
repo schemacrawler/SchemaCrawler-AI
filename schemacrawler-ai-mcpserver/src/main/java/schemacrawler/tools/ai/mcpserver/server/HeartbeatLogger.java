b718c02c9938af7547e0940eb530474a3c162ee4
Sualeh Fatehi <sualeh@hotmail.com>
2025-07-30 20:58:41 -0400

    Make configuration common

--- schemacrawler-ai-mcpserver/src/main/java/schemacrawler/tools/ai/mcpserver/server/HeartbeatLogger.java
+++ schemacrawler-ai-mcpserver/src/main/java/schemacrawler/tools/ai/mcpserver/server/HeartbeatLogger.java
@@ -13,11 +13,10 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import schemacrawler.Version;
 import us.fatehi.utility.string.StringFormat;
 
 @Component
@@ -26,27 +25,25 @@ public class HeartbeatLogger {
 
   private static final Logger LOGGER = Logger.getLogger(HeartbeatLogger.class.getCanonicalName());
 
-  private final Environment environment;
-
-  public HeartbeatLogger(final Environment environment) {
-    this.environment = environment;
-  }
+  @Value("${server.heartbeat}")
+  private boolean heartbeat;
+  @Value("${server.name}")
+  private String serverName;
+  @Value("${server.version}")
+  private String serverVersion;
 
   @Scheduled(timeUnit = TimeUnit.SECONDS, fixedRate = 30)
   public void logHeartbeat() {
-    final boolean heartbeat = Boolean.valueOf(environment.getProperty("server.heartbeat", "false"));
     if (!heartbeat) {
       return;
     }
 
-    final String serverName = environment.getProperty("server.name", "SchemaCrawler MCP Server");
-    final String serverVersion = Version.version().toString();
     final long uptime = ManagementFactory.getRuntimeMXBean().getUptime() / 1_000;
     final Duration duration = Duration.ofSeconds(uptime);
 
     LOGGER.log(
         Level.INFO,
         new StringFormat(
-            "Heartbeat: %s (%s) is running. Uptime %s.", serverName, serverVersion, duration));
+            "Heartbeat: %s %s is running; uptime %s.", serverName, serverVersion, duration));
   }
 }
