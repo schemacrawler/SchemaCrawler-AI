9a21af75f1656e4bcf8f717c62d942b1dfd3a2d0
Sualeh Fatehi <sualeh@hotmail.com>
2025-09-20 21:46:41 -0400

    Add MCP classes directly

--- schemacrawler-ai-mcpserver/src/main/java/schemacrawler/tools/ai/mcpserver/server/ToolHelper.java
+++ schemacrawler-ai-mcpserver/src/main/java/schemacrawler/tools/ai/mcpserver/server/ToolHelper.java
@@ -10,27 +10,99 @@ package schemacrawler.tools.ai.mcpserver.server;
 
 import static java.util.Objects.requireNonNull;
 
+import io.modelcontextprotocol.server.McpServerFeatures;
+import io.modelcontextprotocol.server.McpSyncServerExchange;
+import io.modelcontextprotocol.spec.McpSchema.CallToolRequest;
+import io.modelcontextprotocol.spec.McpSchema.CallToolResult;
+import io.modelcontextprotocol.spec.McpSchema.TextContent;
+import io.modelcontextprotocol.spec.McpSchema.Tool;
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.logging.Logger;
-import org.springframework.ai.tool.definition.ToolDefinition;
+import org.springframework.ai.model.ModelOptionsUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import schemacrawler.schema.Catalog;
+import schemacrawler.tools.ai.tools.FunctionCallback;
 import schemacrawler.tools.ai.tools.FunctionDefinition;
+import schemacrawler.tools.ai.tools.FunctionReturn;
 import schemacrawler.tools.ai.tools.ToolUtility;
 
 @Component
 public class ToolHelper {
 
+  @Deprecated
+  public static class DeprecatedToolCallHandler
+      implements BiFunction<McpSyncServerExchange, Map<String, Object>, CallToolResult> {
+
+    private ToolCallHandler toolCallHander;
+
+    public DeprecatedToolCallHandler(ToolCallHandler toolCallHander) {
+      toolCallHander = requireNonNull(toolCallHander, "No tool call handler provided");
+    }
+
+    @Override
+    public CallToolResult apply(
+        final McpSyncServerExchange exchange, final Map<String, Object> request) {
+      return toolCallHander.apply(exchange, new CallToolRequest(toolCallHander.name(), request));
+    }
+  }
+
+  public static class ToolCallHandler
+      implements BiFunction<McpSyncServerExchange, CallToolRequest, CallToolResult> {
+
+    private final FunctionCallback functionCallback;
+
+    public ToolCallHandler(final FunctionCallback functionCallback) {
+      this.functionCallback = requireNonNull(functionCallback, "No function callback provided");
+    }
+
+    @Override
+    public CallToolResult apply(
+        final McpSyncServerExchange exchange, final CallToolRequest request) {
+      try {
+        final String arguments = ModelOptionsUtils.toJsonString(request.arguments());
+        final Connection connection = ConnectionService.getConnection();
+        final FunctionReturn functionReturn = functionCallback.execute(arguments, connection);
+        return new CallToolResult(List.of(new TextContent(functionReturn.get())), false);
+      } catch (final Exception e) {
+        return new CallToolResult(List.of(new TextContent(e.getMessage())), true);
+      }
+    }
+
+    public String name() {
+      return functionCallback.getFunctionName().getName();
+    }
+  }
+
   private static final Logger LOGGER = Logger.getLogger(ToolHelper.class.getCanonicalName());
 
-  public ToolDefinition toToolDefinition(final FunctionDefinition<?> functioDefinition) {
-    requireNonNull(functioDefinition, "No function definition provided");
-    final ToolDefinition toolDefinition =
-        ToolDefinition.builder()
-            .name(functioDefinition.getName())
-            .description(functioDefinition.getDescription())
+  @Autowired private Catalog catalog;
+
+  public McpServerFeatures.SyncToolSpecification toSyncToolSpecification(
+      final FunctionDefinition<?> functionDefinition) {
+
+    final Tool tool = toTool(functionDefinition);
+    final FunctionCallback functionCallback =
+        new FunctionCallback(functionDefinition.getName(), catalog);
+    final ToolCallHandler toolCallHandler = new ToolCallHandler(functionCallback);
+
+    return new McpServerFeatures.SyncToolSpecification(
+        tool, new DeprecatedToolCallHandler(toolCallHandler), toolCallHandler);
+  }
+
+  private Tool toTool(final FunctionDefinition<?> functionDefinition) {
+    requireNonNull(functionDefinition, "No function definition provided");
+    final Tool tool =
+        Tool.builder()
+            .name(functionDefinition.getName())
+            .description(functionDefinition.getDescription())
             .inputSchema(
-                ToolUtility.extractParametersSchemaNode(functioDefinition.getParametersClass())
+                ToolUtility.extractParametersSchemaNode(functionDefinition.getParametersClass())
                     .toString())
             .build();
-    return toolDefinition;
+    return tool;
   }
 }
