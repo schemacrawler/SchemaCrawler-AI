eddb96675b06febc34f39442cd22203b903ea99e
Sualeh Fatehi <sualeh@hotmail.com>
2025-11-17 18:45:36 -0500

    Add test for parameters JSON schema

--- schemacrawler-ai-core/src/test/java/schemacrawler/tools/ai/tools/test/JsonSchemaToolUtilityTest.java
+++ schemacrawler-ai-core/src/test/java/schemacrawler/tools/ai/tools/test/JsonSchemaToolUtilityTest.java
@@ -13,15 +13,12 @@ import static us.fatehi.test.utility.extensions.FileHasContent.classpathResource
 import static us.fatehi.test.utility.extensions.FileHasContent.hasSameContentAs;
 import static us.fatehi.test.utility.extensions.FileHasContent.outputOf;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.TreeMap;
-
 import org.junit.jupiter.api.Test;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
 import schemacrawler.tools.ai.functions.DescribeRoutinesFunctionParameters;
 import schemacrawler.tools.ai.functions.DescribeTablesFunctionParameters;
 import schemacrawler.tools.ai.functions.DiagramFunctionParameters;
@@ -35,29 +32,35 @@ import us.fatehi.test.utility.TestWriter;
 
 public class JsonSchemaToolUtilityTest {
 
-	@Test
-	public void functionParameters() {
+  @Test
+  public void functionParameters() {
 
-		List<Class<? extends FunctionParameters>> functionParameters = List.of(DescribeTablesFunctionParameters.class,
-				DescribeRoutinesFunctionParameters.class, LintFunctionParameters.class, ListFunctionParameters.class,
-				ListAcrossTablesFunctionParameters.class, DiagramFunctionParameters.class,
-				TableSampleFunctionParameters.class);
+    final List<Class<? extends FunctionParameters>> functionParameters =
+        List.of(
+            DescribeTablesFunctionParameters.class,
+            DescribeRoutinesFunctionParameters.class,
+            LintFunctionParameters.class,
+            ListFunctionParameters.class,
+            ListAcrossTablesFunctionParameters.class,
+            DiagramFunctionParameters.class,
+            TableSampleFunctionParameters.class);
 
-		final TestWriter testout = new TestWriter();
-		try (final TestWriter out = testout) {
-			for (final Class<?> parametersClass : functionParameters) {
-				SortedMap<String, JsonNode> parametersSchemaMap = new TreeMap<>(
-						ToolUtility.extractParametersSchemaMap(parametersClass));
+    final TestWriter testout = new TestWriter();
+    try (final TestWriter out = testout) {
+      for (final Class<?> parametersClass : functionParameters) {
+        final SortedMap<String, JsonNode> parametersSchemaMap =
+            new TreeMap<>(ToolUtility.extractParametersSchemaMap(parametersClass));
 
-				out.println(parametersClass.getSimpleName());
-				for (Entry<String, JsonNode> parameter : parametersSchemaMap.entrySet()) {
-					out.println(parameter.getKey().indent(2).stripTrailing() + ": ");
-					out.println(parameter.getValue().toPrettyString().indent(4).stripTrailing() );
-				}
-				out.println();
-				out.println();
-			}
-		}
-		assertThat(outputOf(testout), hasSameContentAs(classpathResource("parameters-json-schemas.txt")));
-	}
+        out.println(parametersClass.getSimpleName());
+        for (Entry<String, JsonNode> parameter : parametersSchemaMap.entrySet()) {
+          out.println(parameter.getKey().indent(2).stripTrailing() + ": ");
+          out.println(parameter.getValue().toPrettyString().indent(4).stripTrailing());
+        }
+        out.println();
+        out.println();
+      }
+    }
+    assertThat(
+        outputOf(testout), hasSameContentAs(classpathResource("parameters-json-schemas.txt")));
+  }
 }
