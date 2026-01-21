3d13f9c6fa237e175f66ceb2765b2a988d6d8ffa
Sualeh Fatehi <sualeh@hotmail.com>
2026-01-20 22:16:31 -0500

    Change license to BUSL (#169)
    * Switch license to BUSL-1.1
    
    * Update license

--- schemacrawler-ai-mcpserver/src/main/bin/schemacrawler-ai.sh
+++ schemacrawler-ai-mcpserver/src/main/bin/schemacrawler-ai.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Copyright (c) Sualeh Fatehi
-# SPDX-License-Identifier: CC-BY-NC-4.0
+# SPDX-License-Identifier: BUSL-1.1
 
 SC_DIR=/opt/schemacrawler
 java -cp "$SC_DIR"/lib/*:"$SC_DIR"/config schemacrawler.tools.ai.mcpserver.McpServerMain "$@"
